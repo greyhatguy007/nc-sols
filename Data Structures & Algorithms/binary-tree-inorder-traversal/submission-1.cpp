0e6210dcefb20ad0886122df36725a4142c3955c
Ritvik <mritvikprasad@gmail.com>
2026-05-06 23:40:32 +0530

    Add: binary-tree-inorder-traversal - submission-1

---
+++ Data Structures & Algorithms/binary-tree-inorder-traversal/submission-1.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        TreeNode* cur = root;
+        stack<TreeNode*> st;
+        while(cur || !st.empty()){
+            while(cur) {
+                st.push(cur);
+                cur = cur -> left;
+            }
+            cur = st.top();
+            ans.push_back(cur->val);
+            st.pop();
+            cur = cur -> right;
+        }
+        return ans;
+    }
+};
